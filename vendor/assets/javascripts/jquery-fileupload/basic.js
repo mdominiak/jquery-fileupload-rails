d6d8ab0badb91ddddeb8e842b696d662e3049381
Mateusz Dominiak <mateusz.dominiak@gmail.com>
2013-02-01 18:39:15 +0100

    Require jquery.ui.widget in basic.js

--- vendor/assets/javascripts/jquery-fileupload/basic.js
+++ vendor/assets/javascripts/jquery-fileupload/basic.js
@@ -1,4 +1,4 @@
-//=require jquery-fileupload/vendor/jquery.ui.widget
+//=require jquery.ui.widget
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
 
